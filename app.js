a668e941f3f021279be80819ed0b7bbc803e6b19
六合 <1332983774@qq.com>
2017-01-25 23:12:35 +0800

    初始设置，数据库设置

--- app.js
+++ app.js
@@ -5,11 +5,25 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');
+//var users = require('./routes/users');
+var settings = require('./settings')
+
+//
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
-
+app.use(session({
+  seret: settings.cookieSecret,
+  key: settings.db, //cookie name
+  cookie: {maxAge: 1000 * 60 * 60 24 *30},
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port:settings.port
+  })
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,8 +36,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+//app.use('/users', users);
+routes(app)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = router;*/
+module.exports = function(app){
+  app.get('./',function(req, res){
+    res.render('index', { title: '主页' });
+  });
+  app.get('./reg',function(req, res){
+    res.render('index', { title: '注册' });
+  });
+  app.post('./reg',function(req, res){
+  });
+  app.get('./login',function(req, res){
+    res.render('index', { title: '登陆' });
+  });
+  app.post('./login',function(req, res){
+    res.render('index', { title: '登陆' });
+  });
+  app.get('./post', function(req, res){
+    res.render('index', { title: '发表' });
+  });
+  app.post('/post', function(req, res){
+  });
+  app.get('/logout',function(req, res){
+  });
+};
