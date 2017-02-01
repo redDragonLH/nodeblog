4f7eac2ee8d46caf6c72e28ac0ebcdb7aa176f36
六合 <1332983774@qq.com>
2017-02-01 10:37:08 +0800

    实现简单留言功能

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
 var Post = require('../models/post.js');
+var Comment = require('../models/comment.js');
 // 上传文件
 var multer = require('multer');
 var storage = multer.diskStorage({
@@ -205,6 +206,27 @@ module.exports = function(app){
         success: req.flash('success').toString(),
         error: req.flash('error').toString()
       });
+    });
+  });
+
+  app.post('/u/:name/:day/:title',function (req, res) {
+    var date = new Date(),
+        time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "" + date.getHours() + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+    var comment = {
+      name: req.body.name,
+      email: req.body.email,
+      website: req.body.website,
+      time: time,
+      content: req.body.content
+    };
+    var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+    newComment.save(function(err) {
+      if(err) {
+        req.flash('error', err);
+        return res.redirect('back');
+      }
+      req.flash('success', '留言成功');
+      res.redirect('back');
     });
   });
 
