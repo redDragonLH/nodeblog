07d4fd91692e72457cf2ad1322007e9dfbba8d03
六合 <1332983774@qq.com>
2017-02-05 12:20:56 +0800

    ...

--- routes/index.js
+++ routes/index.js
@@ -431,7 +431,7 @@ module.exports = function(app){
   });
 
   function checkLogin(req, res, next) {
-    if( req.session.user === undefined ) {
+    if( !req.session.user) {
       req.flash('error', '未登录');
       res.redirect('/login');
     }
