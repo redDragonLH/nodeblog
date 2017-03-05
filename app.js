074989c40c874ff278da55b17377d2a74975c3c4
六合 <1332983774@qq.com>
2017-03-05 23:18:06 +0800

    ...

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 var settings = require('./settings');
