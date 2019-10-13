395285fff8f7ce443dccee4b36384411d4ac0c14
刘鹤 <ok1332983774@163.com>
2019-10-13 15:21:02 +0800

    loca DB

--- models/locadb.js
+++ models/locadb.js
@@ -1,1 +1,38 @@
-
+const DB = {
+    artice:{},
+    user:{},
+    articeList:{},
+}
+class LocaDB {
+    constructor() {
+        
+    }
+    insert(name,data){
+        if(name === 'artice'){
+            this.articeInsert(data)
+        }
+    }
+    articeInsert(data){
+        DB.artice[data.title + data.user + data.time.day] = data
+        DB.articeList[data.user][data.title]= {
+                                                title : data.title,
+                                                time : data.time,
+                                                tag: data.tag,
+                                            }
+    }
+    find(flag,info={}, callback){
+        let data = []
+        
+        for (const ar of DB.artice) {
+            if(ar[flag].includes(info.flagCen)){
+                data.push(ar)
+            }
+        }
+        callback(null,data)
+    }
+    getArtice(name, day, title, callback){
+        DB.artice[title + name + day].pv++;
+        callback(null, DB.artice[title + name + day])
+    }
+}
+module.exports = new LocaDB();
